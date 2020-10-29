9defd16f50cf0d059691fb49a811b9eebf589dc6
kishor <kishorj007@gmail.com>
2020-10-28 22:29:07 -0400

    fix fail

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -6,13 +6,6 @@ import com.pg.utils.utility as ut
 
 if __name__ == '__main__':
 
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read ingestion enterprise applications") \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
@@ -21,6 +14,15 @@ if __name__ == '__main__':
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
@@ -68,9 +70,9 @@ if __name__ == '__main__':
 
             cust_addr_df.show()
 
-            cust_addr_df.write \
-                .mode("overwrite") \
-                .partitionBy("ins_dt") \
-                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
+            # cust_addr_df.write \
+            #     .mode("overwrite") \
+            #     .partitionBy("ins_dt") \
+            #     .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
 
 # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/pg/source_data_loading.py
