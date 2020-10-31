6253a221aa880bd4700ef96b052d1ef1a7a9134c
kishor <kishorj007@gmail.com>
2020-10-31 09:40:17 -0400

    add date function

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -38,6 +38,7 @@ if __name__ == '__main__':
     Customer_df.printSchema()
     Customer_df.show(5, False)
     Customer_df.createOrReplaceTempView("CustomerPortal")
+    print(current_date() - 2)
 
     spark.sql("""SELECT 
                    DISTINCT REGIS_CNSM_ID, CAST(REGIS_CTY_CODE AS SMALLINT), CAST(REGIS_ID AS INTEGER),
@@ -57,6 +58,7 @@ if __name__ == '__main__':
     Address_df.printSchema()
     Address_df.show(5, False)
     Address_df.createOrReplaceTempView("Address")
+    print(current_date() - 2)
 
 
     spark.sql("""SELECT 
