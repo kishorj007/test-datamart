69fd534acf9622795ecebce6f6e7a3c3cba0997a
kishor <kishorj007@gmail.com>
2020-11-01 22:18:23 -0500

    test

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -23,14 +23,6 @@ if __name__ == '__main__':
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
-
-    # Read CP and addr data for current date
-    # create temp view on top of that
-    # execute a spark sql query to get the dim table data
-
-
-    # Read CP data for current date
-
     print("\nStart reading data from CP parque file")
 
     Customer_file_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + "CP"
@@ -38,18 +30,6 @@ if __name__ == '__main__':
     Customer_df.printSchema()
     Customer_df.show(5, False)
     Customer_df.createOrReplaceTempView("CustomerPortal")
-    #print(date_add(current_date() - 2))
-
-    spark.sql("""SELECT 
-                   DISTINCT REGIS_CNSM_ID, CAST(REGIS_CTY_CODE AS SMALLINT), CAST(REGIS_ID AS INTEGER),
-                   REGIS_LTY_ID, REGIS_DATE, REGIS_CHANNEL, REGIS_GENDER, REGIS_CITY, INS_DT
-                FROM
-                  CustomerPortal
-                WHERE
-                  INS_DT = '2020-10-29'""")\
-        .show(5, False)
-
-    # Read addr data for current date
 
     print("\nStart reading data from Address parque file")
 
@@ -58,33 +38,11 @@ if __name__ == '__main__':
     Address_df.printSchema()
     Address_df.show(5, False)
     Address_df.createOrReplaceTempView("Address")
-    #print(date_add(current_date() - 2))
 
-
-    spark.sql("""SELECT 
-                   DISTINCT consumer_id,'mobile-no' ,state    ,city     ,street,ins_dt
-                FROM
-                  Address
-                WHERE
-                  INS_DT = '2020-10-29'""")\
-        .show(5, False)
 
     print("join examples")
 
-#     spark.sql("""SELECT
-#                     DISTINCT a.REGIS_CNSM_ID, CAST(a.REGIS_CTY_CODE AS SMALLINT), CAST(a.REGIS_ID AS INTEGER),
-#                    a.REGIS_LTY_ID, a.REGIS_DATE, a.REGIS_CHANNEL, a.REGIS_GENDER, a.REGIS_CITY,
-#                     b.state, b.city,
-#                    b.street,b.ins_dt
-#                 FROM
-#                   CustomerPortal a join Address b
-#                   on (a.REGIS_LTY_ID=b.consumer_id)""")\
-#         .show(5, False)
-#
-# print("Completed   <<<<<<<<<")
-
-
-spark.sql(app_conf["REGIS_DIM"]["loadingQuery"]) \
+    spark.sql(app_conf["REGIS_DIM"]["loadingQuery"]) \
         .show(5, False)
 
 # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" com/pg/target_data_loading.py
