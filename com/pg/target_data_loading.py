29d61341ac3a97feccf17cc6d230ea2d45dfe16b
kishor <kishorj007@gmail.com>
2020-11-07 10:30:51 -0500

    test

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
-
     def fn_uuid():
         uid = uuid.uuid1()
         return str(uid)
@@ -34,9 +33,7 @@ if __name__ == '__main__':
         .register("FN_UUID", fn_uuid, StringType())
 
     tgt_list = app_conf['target_list']
-    df = spark.createDataFrame([1, 2], IntegerType())
-    df.createOrReplaceTempView("df")
-    spark.sql("select *, FN_UUID() uuid from df").show()
+
     for tgt in tgt_list:
         tgt_conf = app_conf[tgt]
 
@@ -59,7 +56,7 @@ if __name__ == '__main__':
                 .option("url", ut.get_redshift_jdbc_url(app_secret)) \
                 .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
                 .option("forward_spark_s3_credentials", "true") \
-                .option("dbtable", "PUBLIC.TXN_FCT") \
+                .option("dbtable", "DATAMART.REGIS_DIM") \
                 .mode("overwrite") \
                 .save()
 
@@ -69,4 +66,4 @@ if __name__ == '__main__':
 
 
 
-# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" com/pg/target_data_loading.py
+# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/target_data_loading.py
