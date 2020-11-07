bfac303c8faeabadd5529fbda803e5129df72346
kishor <kishorj007@gmail.com>
2020-11-07 09:13:37 -0500

    test

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -33,7 +33,10 @@ if __name__ == '__main__':
         .register("FN_UUID", fn_uuid, StringType())
 
     tgt_list = app_conf['target_list']
-
+    df = spark.sparkContext.parallelize([1, 2]).toDF("id")
+    df.createOrReplaceTempView("df")
+    df.withColumn('uuid', FN_UUID()).show()
+    spark.sql("select id, FN_UUID() uuid from df").show()
     for tgt in tgt_list:
         tgt_conf = app_conf[tgt]
 
